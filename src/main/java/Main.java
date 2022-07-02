6400a67cbb6220ed4692b0f8fa1e79c7c0723158
nikito <aa@aa.aa>
2022-07-02 12:13:11 +0300

    four first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int result = sum(10,10);
+        System.out.println(result);
+    }
+
+    static int sum(int a, int b){
+        return a+b;
+    }
+
+}
