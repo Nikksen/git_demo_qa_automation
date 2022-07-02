183e21c512f7a62807e879ff5d982871eaab34dd
nikito <aa@aa.aa>
2022-07-02 12:14:44 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
         int result = sum(10,10);
+        int resultDivision  = division(20,2);
+        System.out.println(resultDivision);
         System.out.println(result);
     }
 
     static int sum(int a, int b){
         return a+b;
     }
+
+    static int division(int a, int b){
+        return a/b;
+    }
+
+
 
 }
